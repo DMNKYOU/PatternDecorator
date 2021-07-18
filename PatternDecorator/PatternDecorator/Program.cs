2a5e673157cbdca3032f1ca2428b8fe2f9d31ffe
DmitriyZalesin <zalesindmitriy@gmail.com>
2021-07-18 23:08:40 +0300

    Init vs proj

---
+++ PatternDecorator/PatternDecorator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PatternDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
